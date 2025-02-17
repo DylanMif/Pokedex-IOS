dfdaadadedb690b375d6adf8cb6cdbbdfafbbacd
DylanMif <dylan.miftari@etu.univ-savoie.fr>
2025-02-17 17:00:56 +0100

    Fix bugs

--- Pokedex/Services/PokemonService.swift
+++ Pokedex/Services/PokemonService.swift
@@ -36,9 +36,11 @@ class PokemonService {
             completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
             return
         }
+        
+        let baseUrl = "https://pokeapi.co/api/v2/pokemon"
+        
+    }
     
-    private let baseUrl = "https://pokeapi.co/api/v2/pokemon"
-
     func fetchPokemonList(offset: Int, limit: Int = 20, completion: @escaping (Result<PokemonListResponse, Error>) -> Void) {
         guard let url = URL(string: "\(baseUrl)?offset=\(offset)&limit=\(limit)") else { return }
         
@@ -54,7 +56,6 @@ class PokemonService {
             }
             
             do {
-                let decodedResponse = try JSONDecoder().decode(T.self, from: data)
                 let decodedResponse = try JSONDecoder().decode(PokemonListResponse.self, from: data)
                 completion(.success(decodedResponse))
             } catch {
@@ -63,20 +64,20 @@ class PokemonService {
         }.resume()
     }
     
-    func searchPokemon(query: String) async throws -> [Pokemon] {
-            // L'API Pokemon ne supporte pas directement la recherche,
-            // donc nous devons charger une plus grande liste et filtrer
-            let limit = (1500)
-            guard let url = URL(string: "\(baseUrl)?limit=\(limit)") else {
-                throw URLError(.badURL)
-            }
-            
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let response = try JSONDecoder().decode(PokemonListResponse.self, from: data)
-            
-            return response.results.filter { pokemon in
-                pokemon.name.lowercased().contains(query.lowercased())
-            }
-        }
     
+    func searchPokemon(query: String) async throws -> [Pokemon] {
+        // L'API Pokemon ne supporte pas directement la recherche,
+        // donc nous devons charger une plus grande liste et filtrer
+        let limit = (1500)
+        guard let url = URL(string: "\(baseUrl)?limit=\(limit)") else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let response = try JSONDecoder().decode(PokemonListResponse.self, from: data)
+        
+        return response.results.filter { pokemon in
+            pokemon.name.lowercased().contains(query.lowercased())
+        }
+    }
 }
