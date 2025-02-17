62deeec8494a110a36ba7c12553646af98693ce4
phtuongvy <tuong-vy.pham-huynh@etu.univ-smb.fr>
2025-02-17 10:42:38 +0100

    Add Core Data + Controller

---
+++ Pokedex/Controllers/Pokemon+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Pokemon+CoreDataClass.swift
+//  Pokedex
+//
+//  Created by Pham Huynh Tuong Vy on 17/02/2025.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Pokemon)
+public class Pokemon: NSManagedObject {
+
+}
