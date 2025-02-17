93cfbd12b1c3f60c4a424f424f74f70b1c3a0ef1
Nicolas <NT.Aergie@gmail.com>
2025-02-17 17:04:02 +0100

    repairing pull

--- Pokedex/Services/PokemonService.swift
+++ Pokedex/Services/PokemonService.swift
@@ -1,10 +1,3 @@
-//
-//  PokemonService.swift
-//  Pokedex
-//
-//  Created by Dylan MIFTARI on 2/17/25.
-//
-
 import Foundation
 
 class PokemonService {
@@ -36,11 +29,6 @@ class PokemonService {
             completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
             return
         }
-    
-    private let baseUrl = "https://pokeapi.co/api/v2/pokemon"
-
-    func fetchPokemonList(offset: Int, limit: Int = 20, completion: @escaping (Result<PokemonListResponse, Error>) -> Void) {
-        guard let url = URL(string: "\(baseUrl)?offset=\(offset)&limit=\(limit)") else { return }
         
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
@@ -55,6 +43,28 @@ class PokemonService {
             
             do {
                 let decodedResponse = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(decodedResponse))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+
+    func fetchPokemonList(offset: Int, limit: Int = 20, completion: @escaping (Result<PokemonListResponse, Error>) -> Void) {
+        guard let url = URL(string: "\(baseUrl)/pokemon?offset=\(offset)&limit=\(limit)") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "", code: -2, userInfo: [NSLocalizedDescriptionKey: "No data received"])))
+                return
+            }
+            
+            do {
                 let decodedResponse = try JSONDecoder().decode(PokemonListResponse.self, from: data)
                 completion(.success(decodedResponse))
             } catch {
@@ -64,19 +74,18 @@ class PokemonService {
     }
     
     func searchPokemon(query: String) async throws -> [Pokemon] {
-            // L'API Pokemon ne supporte pas directement la recherche,
-            // donc nous devons charger une plus grande liste et filtrer
-            let limit = (1500)
-            guard let url = URL(string: "\(baseUrl)?limit=\(limit)") else {
-                throw URLError(.badURL)
-            }
-            
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let response = try JSONDecoder().decode(PokemonListResponse.self, from: data)
-            
-            return response.results.filter { pokemon in
-                pokemon.name.lowercased().contains(query.lowercased())
-            }
+        // L'API Pokemon ne supporte pas directement la recherche,
+        // donc nous devons charger une plus grande liste et filtrer
+        let limit = (1500)
+        guard let url = URL(string: "\(baseUrl)/pokemon?limit=\(limit)") else {
+            throw URLError(.badURL)
         }
-    
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let response = try JSONDecoder().decode(PokemonListResponse.self, from: data)
+        
+        return response.results.filter { pokemon in
+            pokemon.name.lowercased().contains(query.lowercased())
+        }
+    }
 }
