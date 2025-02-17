904202ddb27e3b3c4961af91e1a6a30797852991
DylanMif <dylan.miftari@etu.univ-savoie.fr>
2025-02-17 09:37:11 +0100

    Recreate struct

---
+++ Pokedex/Models/PokemonStruct.swift
@@ -0,0 +1,21 @@
+//
+//  PokemonStruct.swift
+//  Pokedex
+//
+//  Created by Dylan MIFTARI on 2/17/25.
+//
+
+import Foundation
+
+struct Pokemon: Identifiable, Decodable {
+    var id: Int
+    var name: String
+    var image: String
+    var type: String
+    var pv: Int
+    var attack: Int
+    var defense: Int
+    var specialAttack: Int
+    var specialDefense: Int
+    var speed: Int
+}
